b537b684765544f2a0c8edf18718e7b3bcdd9a6f
peteflorence <pete.florence@gmail.com>
2015-10-15 17:44:19 -0400

    1 instance of Obstacle successfully floating around

--- _RotatingBot/Obstacle.py
+++ _RotatingBot/Obstacle.py
@@ -7,7 +7,21 @@ class Obstacle:
         self.centerX = x
         self.centerY = y
         self.radius = 1
-        
+
+        self.Theta_to_center = math.atan2(self.centerX, self.centerY)
+        self.R_to_center = math.sqrt(self.centerX**2 + self.centerY**2)
+    
+    def computethetaTraj(self,psiworld):
+        self.thetaTraj = psiworld + self.Theta_to_center
+
+    def computexyTraj(self,xworld,yworld):
+        self.xtraj = xworld + self.R_to_center*np.cos(self.thetaTraj)
+        self.ytraj = yworld + self.R_to_center*np.sin(self.thetaTraj)
+
+    def computeTraj(self,psiworld,xworld,yworld):
+        self.computethetaTraj(psiworld)
+        self.computexyTraj(xworld,yworld)
+
     def printCenter(self):
         print self.centerX, self.centerY
         
