09663598d8d9e3b6cc102c351768269cf1fa05c3
peteflorence <pete.florence@gmail.com>
2015-10-15 18:33:45 -0400

    random obstacles floating around :)

--- _RotatingBot/ObstacleField.py
+++ _RotatingBot/ObstacleField.py
@@ -1,6 +1,8 @@
 import numpy as np
 import math
 
+from Obstacle import Obstacle
+
 class ObstacleField:
 
     def __init__(self):
@@ -9,6 +11,18 @@ class ObstacleField:
     def addObstacle(self,obstacle):
         self.ObstaclesList.append(obstacle)
     
+    def randomField(self, M=5, mindistance = 4, maxdistance = 17):
+        # M is number of obstacles 
+
+        randTheta = 2*math.pi * np.random.rand(1,M)[0]
+        randR = (maxdistance - mindistance) * np.random.rand(1,M)[0] + mindistance
+
+        for i in range(M):
+            x = randR[i]*math.cos(randTheta[i])
+            y = randR[i]*math.sin(randTheta[i])
+            obs = Obstacle(x,y)
+            self.addObstacle(obs)
+
     def printObstacles(self):
         counter = 1
         for i in self.ObstaclesList:
