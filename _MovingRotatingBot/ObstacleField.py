e84773559b3ee53bb002262146f0797dfe4e9280
peteflorence <pete.florence@gmail.com>
2015-10-17 13:22:03 -0400

    adjusted obstacle field for plane

--- _MovingRotatingBot/ObstacleField.py
+++ _MovingRotatingBot/ObstacleField.py
@@ -11,17 +11,19 @@ class ObstacleField:
     def addObstacle(self,obstacle):
         self.ObstaclesList.append(obstacle)
     
-    def randomField(self, M=5, mindistance = 4, maxdistance = 17):
+    def randomField(self, M=5, mindistance = 4, maxdistance = 100):
         # M is number of obstacles 
 
-        randTheta = 2*math.pi * np.random.rand(1,M)[0]
-        randR = (maxdistance - mindistance) * np.random.rand(1,M)[0] + mindistance
+        # -50 to 50
+        # - 20 to 100
+
+        randx = -50 + 100*np.random.rand(1,M)[0]
+        randy = -20 + 120*np.random.rand(1,M)[0]
 
         for i in range(M):
-            x = randR[i]*math.cos(randTheta[i])
-            y = randR[i]*math.sin(randTheta[i])
-            obs = Obstacle(x,y)
-            self.addObstacle(obs)
+            if math.sqrt(randx[i]**2 + randy[i]**2) > mindistance:
+                obs = Obstacle(randx[i],randy[i])
+                self.addObstacle(obs)
 
     def printObstacles(self):
         counter = 1
